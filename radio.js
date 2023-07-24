1214142d6ef112fac8becc795f908187dedb3f6a
Garuda Anon Security <139625513+GANOSEC@users.noreply.github.com>
2023-07-24 18:57:48 +0700

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,17 +1,35 @@
 let isPlaying = false;
 const musicButton = document.getElementById('musicButton');
 const music = document.getElementById('music');
+let hasUserInteracted = false;
 
 function init() {
   musicButton.addEventListener('click', toggleMusic);
+  document.addEventListener('click', () => {
+    if (!hasUserInteracted) {
+      // Attempt to play the audio on the first user interaction
+      hasUserInteracted = true;
+      playAudio();
+    }
+  });
+}
+
+function playAudio() {
+  if (!isPlaying) {
+    music.play().then(() => {
+      musicButton.classList.add('music-spin');
+      isPlaying = true;
+    }).catch((error) => {
+      // Autoplay was prevented, handle the error gracefully
+      console.error('Autoplay prevented:', error);
+    });
+  }
 }
 
 function toggleMusic() {
   if (isPlaying) {
     music.pause();
     music.currentTime = 0;
-  } else {
-    music.play();
   }
 
   musicButton.classList.toggle('music-spin');
